5a33f855ca3e014449b1c00eb46bb511224f5d68
Arron Chapman <unkwntech@gmail.com>
2025-01-18 22:43:57 -0600

    Adding CI/CD pipeline

---
+++ ecosystem.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    apps: [
+        {
+            name: "Giveaway-Frontend",
+            script: "dist/server/entry.mjs",
+            time: true,
+            instances: 1,
+            autorestart: true,
+            max_restarts: 50,
+            watch: false,
+            max_memory_restart: "1G",
+        },
+    ],
+    deploy: {
+        production: {
+            user: "github",
+            host: "vindi.best",
+            key: "deploy.key",
+            ref: "origin/main",
+            repo: "https://github.com/beautifulmim/fanfest.git",
+            path: "/var/projects/omega-giveaway-frontend-prod/",
+            "post-deploy":
+                "npm i && npm run build && pm2 reload ecosystem.config.js --env production --force && pm2 save",
+            env: {
+                NODE_ENV: "production",
+            },
+        },
+    },
+};
